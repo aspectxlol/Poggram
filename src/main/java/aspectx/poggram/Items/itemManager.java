cbc5c4f8bca2f4940bcdca0fb3ecf37b51a53c39
aspectx <gamernxt6@gmail.com>
2022-04-06 21:29:21 +0700

    make starter pickaxe unbreakable & removed random event

--- src/main/java/aspectx/poggram/Items/itemManager.java
+++ src/main/java/aspectx/poggram/Items/itemManager.java
@@ -1,6 +1,8 @@
 package aspectx.poggram.Items;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -24,6 +26,9 @@ public class itemManager {
         lore.add("");
         lore.add("§6Legendary");
         meta.setLore(lore);
+        meta.setUnbreakable(true);
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
         item.setItemMeta(meta);
 
         StarterPickaxe = item;
